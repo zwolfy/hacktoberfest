04e26ae1d939d20a54e2f35112f3426718156e4a
zwolfy <amanjotjo@gmail.com>
2017-10-25 02:32:10 -0400

    Create HelloWorld.java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,10 @@
+// Author: Joty Kaur
+// Language: Java
+// GitHub: https://github.com/zwolfy
+
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!!!");
+  }
+}
